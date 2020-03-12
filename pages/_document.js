dfab5bb8f30400f5b5b9ac4d9b8c8d982e47cad5
Stuart McCamley <s.mccamley@gmail.com>
2020-03-12 23:04:06 +0000

    downgrading node to test heroku

--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,27 @@
-import Document, { Head, Main, NextScript } from "next/document";
-
+import Document from "next/document"
+import { ServerStyleSheet } from "styled-components"
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
-    // Returns an object like: { html, head, errorHtml, chunks, styles }
-    return renderPage();
-  }
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        })
+      const initialProps = await Document.getInitialProps(ctx)
 
-  render() {
-    return (
-      <html>
-        <Head>
-          <title>My page</title>
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    );
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 }
